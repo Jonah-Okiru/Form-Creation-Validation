7270baeefbd7783bbad61d7639180d8770a06145
Jonah Okiru <okirujonah@gmail.com>
2024-10-14 10:52:16 +0300

    update on scrpt.js file

--- script.js
+++ script.js
@@ -8,45 +8,46 @@ document.addEventListener('DOMContentLoaded', function(){
     // 1. Form Submission Event Listener
     form.addEventListener('submit', (event)=>{
         event.preventDefault();
+        // Input Retrieval and Trimming
+        // username, email and password input
+        const username =document.getElementById('username').value.trim();
+        const email =document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim()
+        // validation logic
+        // Initialize validation variables
+        let isValid = true;
+        const messages = [];
+        // username validation
+        if(username.length <3){
+            isValid= false;
+            messages.push('username should have atleast 3 characters');
+        }
+        // email validation
+        if(!email.includes('@') || !email.includes('.')){
+            isValid = false;
+            messages.push('Kindly enter avalid email address');
+        }
+        //password validation
+        if(password.length <8){
+            isValid=false;
+            messages.push('password should have atleast 8 characters');
+        }
+        //Displaying feedback
+        // Make feedbackDiv visible by setting its style.display to "block".
+        feedbackDiv.style.display= "block";
+        // If isValid remains true, set the textContent of feedbackDiv to "Registration successful!" and its style.color to "#28a745".
+        if(isValid){
+            feedbackDiv.textContent = "Registration successful";
+            feedbackDiv.style.color = "#28a745";
+        } else{
+            feedbackDiv.innerHTML = messages.join("<br>");
+            feedbackDiv.style.color = "#dc3545";
+        }
+        //If isValid is false, join messages with <br> to form a single string, 
+        //and assign this to the innerHTML of feedbackDiv. Set feedbackDiv.style.color to "#dc3545".
     });
-    // Input Retrieval and Trimming
-    // username, email and password input
-    const username =document.getElementById('username').value.trim();
-    const email =document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim()
-    // validation logic
-    // Initialize validation variables
-    let isValid = true;
-    let messages = [];
-    // username validation
-    if(username.length <3){
-        isValid= false;
-        messages.push('username should have atleast 3 characters');
-    }
-    // email validation
-    if(!email.includes('@','.')){
-        isValid = false;
-        messages.push('invalid email');
-    }
-    //password validation
-    if(password.length <8){
-        isValid=false;
-        messages.push('password should have atleast 8 characters')
-    }
-    //Displaying feedback
-    // Make feedbackDiv visible by setting its style.display to "block".
-    feedbackDiv.style.display= "block";
-    // If isValid remains true, set the textContent of feedbackDiv to "Registration successful!" and its style.color to "#28a745".
-    if(isValid=true){
-        feedbackDiv.textContent = "Registration successful";
-        feedbackDiv.style.color = "#28a745";
-    }
-    //If isValid is false, join messages with <br> to form a single string, 
-    //and assign this to the innerHTML of feedbackDiv. Set feedbackDiv.style.color to "#dc3545".
-    if(isValid=false){
-        feedbackDiv.innerHTML = messages.join("<br>");
-        feedbackDiv.style.color = "#dc3545";
-    }
+    
+    
     
 
-})
+});
